03d999d178c1aba62d6d143b6191f0d45c1cd748
Rúben Garcia <rube200@users.noreply.github.com>
2021-02-17 00:57:01 +0000

    Show db type when plugin loads

--- OpenMod.Economy/Economy.cs
+++ OpenMod.Economy/Economy.cs
@@ -1,9 +1,12 @@
 ﻿#region
 
 using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
 using OpenMod.API.Plugins;
 using OpenMod.Core.Plugins;
+using OpenMod.Economy.API;
 
 #endregion
 
@@ -17,9 +20,14 @@ namespace OpenMod.Economy
     {
         internal readonly IStringLocalizer StringLocalizer;
 
-        public Economy(IServiceProvider serviceProvider, IStringLocalizer stringLocalizer) : base(serviceProvider)
+        // ReSharper disable once SuggestBaseTypeForParameter
+        public Economy(ILogger<Economy> logger, IServiceProvider serviceProvider, IStringLocalizer stringLocalizer) :
+            base(serviceProvider)
         {
             StringLocalizer = stringLocalizer;
+            var storeType = Configuration.GetSection("Database:Store_Type").Get<string>();
+            var dbStoreType = Enum.TryParse<StoreType>(storeType, true, out var dbType) ? dbType : StoreType.DataStore;
+            logger.LogInformation($"Database type set to: '{dbStoreType}'");
         }
     }
 }
