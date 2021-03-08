4c2bd111bd84880dcb17edb790e19413d33db6eb
Rúben Garcia <rube200@users.noreply.github.com>
2021-03-08 01:51:46 +0000

    Update commands
    More stringlocalizer options
    Small optimization
    More permissions for pay: bank & negative

--- OpenMod.Economy/Commands/CommandBalance.cs
+++ OpenMod.Economy/Commands/CommandBalance.cs
@@ -10,7 +10,6 @@ using OpenMod.API.Prioritization;
 using OpenMod.API.Users;
 using OpenMod.Core.Commands;
 using OpenMod.Core.Permissions;
-using OpenMod.Core.Users;
 using OpenMod.Extensions.Economy.Abstractions;
 
 #endregion
@@ -29,55 +28,44 @@ namespace OpenMod.Economy.Commands
 
         private readonly IEconomyProvider m_EconomyProvider;
         private readonly IStringLocalizer m_StringLocalizer;
-        private readonly IUserManager m_UserManager;
 
         public CommandBalance(IEconomyProvider economyProvider,
-            IServiceProvider serviceProvider, IStringLocalizer stringLocalizer, IUserManager userManager) : base(
+            IServiceProvider serviceProvider, IStringLocalizer stringLocalizer) : base(
             serviceProvider)
         {
             m_EconomyProvider = economyProvider;
             m_StringLocalizer = stringLocalizer;
-            m_UserManager = userManager;
         }
 
         protected override async Task OnExecuteAsync()
         {
-            if (Context.Actor.Type == KnownActorTypes.Console && Context.Parameters.Length == 0)
-                throw new CommandWrongUsageException(Context);
-
-            var other = false;
-            var targetData = (IUser) null; //i know, blame CS0165
-
-            if (Context.Parameters.Length > 0)
+            if (Context.Parameters.Length > 0 && await CheckPermissionAsync(OthersPerm) == PermissionGrantResult.Grant)
             {
-                var otherPermission = await CheckPermissionAsync(OthersPerm) == PermissionGrantResult.Grant;
-                var target = await Context.Parameters.GetAsync<string>(0);
-                targetData =
-                    await m_UserManager.FindUserAsync(KnownActorTypes.Player, target, UserSearchMode.FindByNameOrId);
+                var targetUser = await Context.Parameters.GetAsync<IUser>(0);
+                if (targetUser is null)
+                    throw new UserFriendlyException(m_StringLocalizer["economy:fail:user_not_found",
+                        new {Input = await Context.Parameters.GetAsync<string>(0)}]);
 
-                if (otherPermission)
+                if (!Context.Actor.Equals(targetUser))
                 {
-                    if (targetData == null)
-                        throw new UserFriendlyException(m_StringLocalizer["economy:fail:user_not_found",
-                            new {Target = target}]);
-
-                    if (!Context.Actor.Id.Equals(targetData.Id, StringComparison.OrdinalIgnoreCase))
-                        other = true;
+                    await PrintAsync(m_StringLocalizer["economy:success:show_balance_other", new
+                    {
+                        Context.Actor,
+                        Balance = await m_EconomyProvider.GetBalanceAsync(targetUser.Id, targetUser.Type),
+                        EconomyProvider = m_EconomyProvider,
+                        Target = Context.Actor
+                    }]);
+                    return;
                 }
             }
 
-            if (!other)
-                targetData =
-                    await m_UserManager.FindUserAsync(Context.Actor.Type, Context.Actor.Id, UserSearchMode.FindById);
-
-            var balance = await m_EconomyProvider.GetBalanceAsync(targetData.Id, targetData.Type);
-            var message = other
-                ? m_StringLocalizer["economy:success:show_balance_other",
-                    new {Balance = balance, m_EconomyProvider.CurrencySymbol, targetData.DisplayName}]
-                : m_StringLocalizer["economy:success:show_balance",
-                    new {Balance = balance, m_EconomyProvider.CurrencySymbol}];
-
-            await PrintAsync(message);
+            await PrintAsync(m_StringLocalizer["economy:success:show_balance",
+                new
+                {
+                    Context.Actor,
+                    Balance = await m_EconomyProvider.GetBalanceAsync(Context.Actor.Id, Context.Actor.Type),
+                    EconomyProvider = m_EconomyProvider
+                }]);
         }
     }
 }
--- OpenMod.Economy/Commands/CommandPay.cs
+++ OpenMod.Economy/Commands/CommandPay.cs
@@ -10,7 +10,6 @@ using OpenMod.API.Prioritization;
 using OpenMod.API.Users;
 using OpenMod.Core.Commands;
 using OpenMod.Core.Permissions;
-using OpenMod.Core.Users;
 using OpenMod.Extensions.Economy.Abstractions;
 
 #endregion
@@ -20,22 +19,26 @@ namespace OpenMod.Economy.Commands
     [Command("pay", Priority = Priority.Normal)]
     [CommandDescription("Pay to a user")]
     [CommandSyntax("<player> <amount> [reason]")]
+    [RegisterCommandPermission(BankAccount,
+        Description = "Any user with this permission pays without withdrawing money from the account")]
+    [RegisterCommandPermission(Negative,
+        Description = "Any user with this permission can withdraw money from other users")]
     [RegisterCommandPermission(PayToSelf, Description = "Permission to increase/decrease the own balance")]
     [UsedImplicitly]
     public class CommandPay : Command
     {
+        public const string BankAccount = "bank";
+        public const string Negative = "negative";
         public const string PayToSelf = "self";
 
         private readonly IEconomyProvider m_EconomyProvider;
         private readonly IStringLocalizer m_StringLocalizer;
-        private readonly IUserManager m_UserManager;
 
         public CommandPay(IEconomyProvider economyProvider, IServiceProvider serviceProvider,
-            IStringLocalizer stringLocalizer, IUserManager userManager) : base(serviceProvider)
+            IStringLocalizer stringLocalizer) : base(serviceProvider)
         {
             m_EconomyProvider = economyProvider;
             m_StringLocalizer = stringLocalizer;
-            m_UserManager = userManager;
         }
 
         protected override async Task OnExecuteAsync()
@@ -43,64 +46,81 @@ namespace OpenMod.Economy.Commands
             if (Context.Parameters.Length < 2) throw new CommandWrongUsageException(Context);
 
             var amount = await Context.Parameters.GetAsync<decimal>(1);
-            var isConsole = Context.Actor.Type == KnownActorTypes.Console;
-            var isNegative = amount < 0;
-
-            var target = await Context.Parameters.GetAsync<string>(0);
-            var targetPlayer =
-                await m_UserManager.FindUserAsync(KnownActorTypes.Player, target, UserSearchMode.FindByNameOrId);
-
-            if (targetPlayer == null)
-                throw new UserFriendlyException(m_StringLocalizer["economy:fail:user_not_found",
-                    new {Target = target}]);
-
-            var selfPermission = false;
-            if (targetPlayer.Id.Equals(Context.Actor.Id))
+            IUser targetUser;
+            try
             {
-                selfPermission = await CheckPermissionAsync(PayToSelf) == PermissionGrantResult.Grant;
-                if (selfPermission)
-                    throw new UserFriendlyException(m_StringLocalizer["economy:fail:self_pay"]);
+                targetUser = await Context.Parameters.GetAsync<IUser>(0);
+            }
+            catch (CommandParameterParseException)
+            {
+                throw new UserFriendlyException(m_StringLocalizer["economy:fail:user_not_found",
+                    new {Input = await Context.Parameters.GetAsync<string>(0)}]);
             }
 
-            if (isNegative && !selfPermission && !isConsole || amount == 0)
+            var self = Context.Actor.Equals(targetUser);
+            if (self && await CheckPermissionAsync(PayToSelf) != PermissionGrantResult.Grant)
+                throw new UserFriendlyException(m_StringLocalizer["economy:fail:self_pay"]);
+
+            if (amount == 0 || !self && amount < 0 &&
+                await CheckPermissionAsync(Negative) != PermissionGrantResult.Grant)
                 throw new UserFriendlyException(m_StringLocalizer["economy:fail:invalid_amount",
                     new {Amount = amount}]);
 
-            var reason = (string) m_StringLocalizer["economy:default:payment_reason"];
-            if (Context.Parameters.Length > 2)
+            string reason;
+            if (Context.Parameters.Length < 3)
+                reason = m_StringLocalizer["economy:default:payment_reason", new
+                {
+                    Context.Actor,
+                    Amount = amount,
+                    EconomyProvider = m_EconomyProvider,
+                    Target = targetUser
+                }];
+            else
                 reason = Context.Parameters.GetArgumentLine(2);
 
             var contextActorBalance = (decimal?) null;
-            if (!isConsole)
+            if (!self && await CheckPermissionAsync(BankAccount) != PermissionGrantResult.Grant)
                 contextActorBalance =
                     await m_EconomyProvider.UpdateBalanceAsync(Context.Actor.Id, Context.Actor.Type, -amount, reason);
 
             var targetBalance =
-                await m_EconomyProvider.UpdateBalanceAsync(targetPlayer.Id, targetPlayer.Type, amount, reason);
-            var printToCaller = m_StringLocalizer[
-                contextActorBalance.HasValue ? "economy:success:pay_player" : "economy:success:pay_console",
-                new
+                await m_EconomyProvider.UpdateBalanceAsync(targetUser.Id, targetUser.Type, amount, reason);
+            if (self)
+            {
+                var printSelf = m_StringLocalizer["economy:success:pay_self", new
                 {
+                    Context.Actor,
+                    Amount = amount,
+                    Balance = targetBalance,
+                    EconomyProvider = m_EconomyProvider,
+                    Target = targetUser
+                }];
+
+                await PrintAsync(printSelf);
+                return;
+            }
+
+            var printToCaller = m_StringLocalizer[
+                contextActorBalance.HasValue ? "economy:success:pay_player" : "economy:success:pay_bank", new
+                {
+                    Context.Actor,
                     Amount = amount,
                     Balance = contextActorBalance ?? targetBalance,
-                    m_EconomyProvider.CurrencyName,
-                    m_EconomyProvider.CurrencySymbol,
-                    targetPlayer.DisplayName
+                    EconomyProvider = m_EconomyProvider,
+                    Target = targetUser
                 }];
             await PrintAsync(printToCaller);
 
-            amount = Math.Abs(amount);
             var printToTarget = m_StringLocalizer[
-                isNegative ? "economy:success:payed_negative" : "economy:success:payed",
-                new
+                amount < 0 ? "economy:success:payed_negative" : "economy:success:payed", new
                 {
-                    Amount = amount,
-                    Balance = targetBalance,
-                    m_EconomyProvider.CurrencyName,
-                    m_EconomyProvider.CurrencySymbol,
-                    Context.Actor.DisplayName
+                    Context.Actor,
+                    Amount = Math.Abs(amount),
+                    Balance = contextActorBalance ?? targetBalance,
+                    EconomyProvider = m_EconomyProvider,
+                    Target = targetUser
                 }];
-            await targetPlayer.PrintMessageAsync(printToTarget);
+            await targetUser.PrintMessageAsync(printToTarget);
         }
     }
 }
