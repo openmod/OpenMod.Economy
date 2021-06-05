e2612690e17b7a83f21769987fcaa720050ff58d
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-05 17:17:49 +0100

    Update Mysql Queries and fix schema on MySql 5.5 or older
    More about:
    https://stackoverflow.com/questions/11847815/error-specified-key-was-too-long-max-key-length-is-1000-bytes

--- OpenMod.Economy/DataBase/MySqlDatabase.cs
+++ OpenMod.Economy/DataBase/MySqlDatabase.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System;
+using System.Data.Common;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Localization;
@@ -24,110 +25,114 @@ namespace OpenMod.Economy.DataBase
             .GetSection("Database:Connection_String")
             .Get<string>();
 
-        public async Task CheckShemasAsync()
+        public Task CheckShemasAsync()
         {
-            await using var connection = new MySqlConnection(ConnectionString);
-            await using var command = connection.CreateCommand();
-            await connection.OpenAsync();
-
-            command.CommandText = $"SHOW TABLES LIKE '{TableName}';";
-            if (await command.ExecuteScalarAsync() != null)
-                return;
-
-            command.CommandText = $"CREATE TABLE `{TableName}` (" +
-                                  "`Id` VARCHAR(255), " +
-                                  "`Type` VARCHAR(20), " +
-                                  "`Balance` DECIMAL NOT NULL, " +
-                                  "PRIMARY KEY(`Id`, `Type`)) " +
-                                  "COLLATE='utf8mb4_general_ci';";
-
-            await command.ExecuteNonQueryAsync();
-        }
-
-        public override async Task<decimal> GetBalanceAsync(string ownerId, string ownerType)
-        {
-            await using var connection = new MySqlConnection(ConnectionString);
-            await using var command = connection.CreateCommand();
-
-            command.Parameters.Add("@ownerid", MySqlDbType.VarChar).Value = ownerId;
-            command.Parameters.Add("@ownertype", MySqlDbType.VarChar).Value = ownerType;
-            command.CommandText = "SELECT `Balance` " +
-                                  $"FROM `{TableName}` " +
-                                  "WHERE `Id` = @ownerid AND `Type` = @ownertype;";
-
-            await connection.OpenAsync();
-            if (await command.ExecuteScalarAsync() is decimal balance) return balance;
-
-            return DefaultBalance;
-        }
-
-        public override async Task<decimal> UpdateBalanceAsync(string ownerId, string ownerType, decimal amount,
-            string _)
-        {
-            await using var connection = new MySqlConnection(ConnectionString);
-            await using var command = connection.CreateCommand();
-            await connection.OpenAsync();
-
-            command.Parameters.Add("@ownerid", MySqlDbType.VarChar).Value = ownerId;
-            command.Parameters.Add("@ownertype", MySqlDbType.VarChar).Value = ownerType;
-            command.Parameters.Add("@amount", MySqlDbType.Decimal).Value = amount;
-
-            //Yet i know
-            while (true)
+            return ExecuteMySqlAsync(async command =>
             {
-                command.CommandText = $"UPDATE `{TableName}` " + "SET `Balance` = `Balance` + @amount " +
-                                      "WHERE `Id` = @ownerid AND `Type` = @ownertype;";
+                command.CommandText = $"SHOW TABLES LIKE '{TableName}';";
+                if (await command.ExecuteScalarAsync() != null)
+                    return;
 
-                if (await command.ExecuteNonQueryAsync() == 0)
-                {
-                    await CreateAccountIntenalAsync(ownerId, ownerType, DefaultBalance);
-                    continue;
-                }
-
-                var balance = await GetBalanceAsync(ownerId, ownerType);
-                if (balance >= 0 || amount >= 0) return balance;
-
-                balance = await UpdateBalanceAsync(ownerId, ownerType, Math.Abs(amount), null);
-                throw new NotEnoughBalanceException(
-                    m_StringLocalizer["economy:fail:not_enough_balance",
-                        new {Balance = balance, EconomyProvider = (IEconomyProvider) this}],
-                    balance);
-            }
+                command.CommandText = $"CREATE TABLE `{TableName}` (" +
+                                      "`Id` VARCHAR(128), " +
+                                      "`Type` VARCHAR(20), " +
+                                      "`Balance` DECIMAL NOT NULL, " +
+                                      "PRIMARY KEY(`Id`, `Type`)) " +
+                                      "COLLATE='utf8mb4_general_ci';";
+                await command.ExecuteNonQueryAsync();
+            });
         }
 
-        public override async Task SetBalanceAsync(string ownerId, string ownerType, decimal balance)
+        private async Task<bool> CreateAccountIntenalAsync(DbCommand command)
         {
-            await using var connection = new MySqlConnection(ConnectionString);
-            await using var command = connection.CreateCommand();
-            await connection.OpenAsync();
-
-            if (await CreateAccountIntenalAsync(ownerId, ownerType, balance))
-                return;
-
-            command.Parameters.Add("@ownerid", MySqlDbType.VarChar).Value = ownerId;
-            command.Parameters.Add("@ownertype", MySqlDbType.VarChar).Value = ownerType;
-            command.Parameters.Add("@balance", MySqlDbType.Decimal).Value = balance;
-            command.CommandText = $"UPDATE `{TableName}` " +
-                                  "SET `Balance` = @balance " +
-                                  "WHERE `Id` = @ownerid AND `Type` = @ownertype;";
-
-            await command.ExecuteNonQueryAsync();
-        }
-
-        private async Task<bool> CreateAccountIntenalAsync(string ownerId, string ownerType, decimal balance)
-        {
-            await using var connection = new MySqlConnection(ConnectionString);
-            await using var command = connection.CreateCommand();
-
-            command.Parameters.Add("@ownerid", MySqlDbType.VarChar).Value = ownerId;
-            command.Parameters.Add("@ownertype", MySqlDbType.VarChar).Value = ownerType;
-            command.Parameters.Add("@balance", MySqlDbType.Decimal).Value = balance;
             command.CommandText = $"INSERT IGNORE INTO `{TableName}` " +
                                   "(`Id`, `Type`, `Balance`) VALUES " +
                                   "(@ownerid, @ownertype, @balance);";
-
-            await connection.OpenAsync();
             return await command.ExecuteNonQueryAsync() > 0;
+        }
+
+        public override Task<decimal> GetBalanceAsync(string ownerId, string ownerType)
+        {
+            return ExecuteMySqlAsync(async command =>
+            {
+                command.Parameters.Add("@ownerid", MySqlDbType.VarChar).Value = ownerId;
+                command.Parameters.Add("@ownertype", MySqlDbType.VarChar).Value = ownerType;
+                command.Parameters.Add("@balance", MySqlDbType.Decimal).Value = DefaultBalance;
+                if (await CreateAccountIntenalAsync(command))
+                    return DefaultBalance;
+
+
+                command.CommandText = "SELECT `Balance` " +
+                                      $"FROM `{TableName}` " +
+                                      "WHERE `Id`=@ownerid AND `Type`=@ownertype;";
+                return await command.ExecuteScalarAsync() as decimal? ?? DefaultBalance;
+            });
+        }
+
+        public override Task SetBalanceAsync(string ownerId, string ownerType, decimal balance)
+        {
+            return ExecuteMySqlAsync(async command =>
+            {
+                command.Parameters.Add("@ownerid", MySqlDbType.VarChar).Value = ownerId;
+                command.Parameters.Add("@ownertype", MySqlDbType.VarChar).Value = ownerType;
+                command.Parameters.Add("@balance", MySqlDbType.Decimal).Value = balance;
+                if (await CreateAccountIntenalAsync(command))
+                    return;
+
+                command.CommandText = $"UPDATE `{TableName}` " +
+                                      "SET `Balance`=@balance " +
+                                      "WHERE `Id`=@ownerid AND `Type`=@ownertype;";
+                await command.ExecuteNonQueryAsync();
+            });
+        }
+
+        public override Task<decimal> UpdateBalanceAsync(string ownerId, string ownerType, decimal amount,
+            string _)
+        {
+            return ExecuteMySqlAsync(async command =>
+            {
+                command.Parameters.Add("@ownerid", MySqlDbType.VarChar).Value = ownerId;
+                command.Parameters.Add("@ownertype", MySqlDbType.VarChar).Value = ownerType;
+                command.Parameters.Add("@amount", MySqlDbType.Decimal).Value = amount;
+
+                var newDefault = DefaultBalance + amount;
+                command.Parameters.Add("@balance", MySqlDbType.Decimal).Value = newDefault;
+
+                if (await CreateAccountIntenalAsync(command))
+                    return newDefault;
+
+                command.CommandText = $"UPDATE `{TableName}` " +
+                                      "SET `Balance`=`Balance`+@amount " +
+                                      "WHERE `Id`=@ownerid " +
+                                      "AND `Type`=@ownertype" +
+                                      $"{(amount < 0 ? " AND `Balance`+@amount>='0'" : string.Empty)};";
+
+                var success = await command.ExecuteNonQueryAsync() > 0;
+                var balance = await GetBalanceAsync(ownerId, ownerType);
+                if (success)
+                    return balance;
+
+                throw new NotEnoughBalanceException(
+                    m_StringLocalizer["economy:fail:not_enough_balance",
+                        new {Amount = amount, Balance = balance, EconomyProvider = (IEconomyProvider) this}],
+                    balance);
+            });
+        }
+
+        private async Task<T> ExecuteMySqlAsync<T>(Func<MySqlCommand, Task<T>> task)
+        {
+            await using var connection = new MySqlConnection(ConnectionString);
+            await using var command = connection.CreateCommand();
+            await connection.OpenAsync();
+            return await task(command);
+        }
+
+        private async Task ExecuteMySqlAsync(Func<MySqlCommand, Task> task)
+        {
+            await using var connection = new MySqlConnection(ConnectionString);
+            await using var command = connection.CreateCommand();
+            await connection.OpenAsync();
+            await task(command);
         }
     }
 }
