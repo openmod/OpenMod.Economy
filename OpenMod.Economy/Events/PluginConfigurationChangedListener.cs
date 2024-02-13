09f1d93f2670a61d94cebb435bf3560c2a705d45
Rúben Garcia <rube200@users.noreply.github.com>
2024-02-13 23:52:03 +0000

    Update PluginConfigurationChangedListener.cs

--- OpenMod.Economy/Events/PluginConfigurationChangedListener.cs
+++ OpenMod.Economy/Events/PluginConfigurationChangedListener.cs
@@ -1,39 +1,40 @@
-﻿#region
-
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Autofac;
 using JetBrains.Annotations;
 using Microsoft.Extensions.Logging;
 using OpenMod.API.Eventing;
 using OpenMod.Core.Plugins.Events;
-using OpenMod.Economy.Controllers;
-using OpenMod.Extensions.Economy.Abstractions;
+using OpenMod.Economy.API;
+using OpenMod.Economy.Extensions;
+using OpenMod.Economy.Models;
 
-#endregion
+namespace OpenMod.Economy.Events;
 
-namespace OpenMod.Economy.Events
+[UsedImplicitly]
+public sealed class PluginConfigurationChangedListener(
+    DatabaseSettings databaseSettings,
+    EconomySettings economySettings,
+    ILogger<Economy> logger) : IEventListener<PluginConfigurationChangedEvent>
 {
-    [UsedImplicitly]
-    public sealed class PluginConfigurationChangedListener : IEventListener<PluginConfigurationChangedEvent>
+    public async Task HandleEventAsync(object? sender, PluginConfigurationChangedEvent @event)
     {
-        private readonly IEconomyProvider m_EconomyProvider;
-        private readonly ILogger<Economy> m_Logger;
+        if (@event.Plugin is not Economy)
+            return;
 
-        public PluginConfigurationChangedListener(IEconomyProvider economyProvider, ILogger<Economy> logger)
+        var oldDbType = databaseSettings.DbType;
+        @event.Configuration.BindConfig("database", databaseSettings, logger);
+        databaseSettings.ConnectionString =
+            databaseSettings.ConnectionString.Replace("{WorkingDirectory}", @event.Plugin.WorkingDirectory);
+
+        if (oldDbType != databaseSettings.DbType)
         {
-            m_EconomyProvider = economyProvider;
-            m_Logger = logger;
+            logger.LogWarning("Changing DB types during runtime({0} -> {1}) (Old database will not be exported).",
+                oldDbType, databaseSettings.DbType);
+
+            var database = @event.Plugin.LifetimeScope.Resolve<IDatabase>();
+            await database.CheckSchemasAsync();
         }
 
-        public async Task HandleEventAsync(object sender, PluginConfigurationChangedEvent @event)
-        {
-            if (@event.Plugin is not Economy)
-                return;
-
-            if (m_EconomyProvider is not DatabaseController databaseController)
-                return;
-
-            await databaseController.ConfigurationChangedBaseAsync();
-            m_Logger.LogInformation($"Config changed! Database type set to: '{databaseController.DbStoreType}'");
-        }
+        @event.Configuration.BindConfig("economy", economySettings, logger);
     }
 }
